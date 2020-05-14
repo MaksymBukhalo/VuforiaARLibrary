903cf106a0c76e576217205312e872c00aaa0763
23nekit <23nekit@gmail.com>
2020-05-14 17:15:54 +0300

    feat(Target) : Add a actions to GameSpawn target Add simple game with balls on this target. Balls will destroy by bullets. Bullets will spawn on taps.

---
+++ Assets/GameSpawn/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public Camera Parent;
+	public GameObject BulletPrefab;
+	public float BulletSpeed = 1000;
+
+	private void Update()
+	{
+		SpawnBulletWhenTapOnScreen();
+	}
+	private void SpawnBulletWhenTapOnScreen()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject NewBullet = Instantiate(BulletPrefab, Parent.transform.position, Quaternion.identity);
+			Ray BulletRay = Parent.ScreenPointToRay(Input.mousePosition);
+			NewBullet.GetComponent<Rigidbody>().AddForce(BulletRay.direction * BulletSpeed, ForceMode.Acceleration);
+		}
+	}
+}
